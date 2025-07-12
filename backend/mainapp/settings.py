8ae8853ab6a1ae71628821ab63df9d27984b43a1
Adityagupta0973 <adityag0908@gmail.com>
2025-07-12 22:56:35 +0530

    Second Commit

--- backend/mainapp/settings.py
+++ backend/mainapp/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/5.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,12 +20,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-v2=p9%4v-q#1h3ew-m=a0t7mam+m1$we(o0^fx&)#qkudl*a#g'
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://medical-insurance-price-predictor-backend.onrender.com']
 
 
 # Application definition
