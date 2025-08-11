e66d51a8dec629a8d07fc484a6f218f848a99edd
Adityagupta0973 <adityag0908@gmail.com>
2025-08-11 22:44:21 +0530

    Added DB URL

--- backend/users/urls.py
+++ backend/users/urls.py
@@ -1,10 +1,9 @@
 from django.urls import path, include
 
-from users.views import APILoginView, APILogoutView, APIPasswordUpdateView
-# create_temp_user  
+from users.views import APILoginView, APILogoutView, APIPasswordUpdateView, create_temp_user  
 urlpatterns = [
     path('login/', APILoginView.as_view(), name='api_login'),
     path('logout/', APILogoutView.as_view(), name='api_logout'),
     path('update_password/', APIPasswordUpdateView.as_view(), name='api_update_password'),
-    # path('create-user/', create_temp_user)
+    path('create-user/', create_temp_user)
 ]
